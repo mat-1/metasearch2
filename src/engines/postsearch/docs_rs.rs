e35a840a5cb622c5f44d2f306d09db6521dbf282
mat <git@matdoes.dev>
2023-12-21 03:57:49 -0600

    fixes for docs.rs engine

--- src/engines/postsearch/docs_rs.rs
+++ src/engines/postsearch/docs_rs.rs
@@ -48,14 +48,12 @@ pub fn parse_response(body: &str) -> Option<String> {
     let doc_html = doc.inner_html();
     let doc_html = ammonia::Builder::default()
         .link_rel(None)
-        .url_relative(ammonia::UrlRelative::RewriteWithBase(
-            Url::parse("https://docs.rs").unwrap(),
-        ))
+        .url_relative(ammonia::UrlRelative::RewriteWithBase(url.clone()))
         .clean(&doc_html)
         .to_string();
 
     Some(format!(
-        r#"<h2>Crate <a href="{url}">{title} {version}</a></h2>
+        r#"<h2>Crate <a href="{url}">{title}</a> <span class="infobox-docs_rs-version">{version}</span></h2>
 <div class="infobox-docs.rs-answer">{doc_html}</div>"#,
         url = html_escape::encode_quoted_attribute(&url.to_string()),
         title = html_escape::encode_text(&title),
