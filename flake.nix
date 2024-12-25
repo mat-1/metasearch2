7d99498781da343283fe5b32759456a5f2bf81e1
caeklol <me@caek.dev>
2024-12-26 00:39:28 +0800

    complete conversion to flake-parts

--- flake.nix
+++ flake.nix
@@ -53,13 +53,13 @@
 
 			checks = {
 				inherit metasearch2;
-			}
+			};
 
 			packages.default = metasearch2;
 
 			apps.default = {
 				type = "app";
-				program = lib.getExe metasearch2;
+				program = "${self.packages.${system}.default}/bin/metasearch";
 			};
 
 			devShells.default = craneLib.devShell {
@@ -75,62 +75,4 @@
 			};
 		};
 	};
-
-#      let
-#        pkgs = nixpkgs.legacyPackages.${system};
-#
-#        craneLib = crane.mkLib pkgs;
-#
-#        assetFilter = path: _type: (pkgs.lib.strings.hasPrefix (toString ./src/web/assets) path);
-#        sourceFilter = path: type: (craneLib.filterCargoSources path type) || (assetFilter path type);
-#
-#        # Common arguments can be set here to avoid repeating them later
-#        # Note: changes here will rebuild all dependency crates
-#        commonArgs = {
-#          src = pkgs.lib.cleanSourceWith {
-#            src = ./.;
-#            filter = sourceFilter;
-#            name = "source"; # Be reproducible, regardless of the directory name
-#          };
-#          strictDeps = true;
-#
-#          buildInputs = [
-#            # Add additional build inputs here
-#          ];
-#        };
-#
-#        metasearch2 = craneLib.buildPackage (commonArgs // {
-#          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-#
-#          # Additional environment variables or build phases/hooks can be set
-#          # here *without* rebuilding all dependency crates
-#          # MY_CUSTOM_VAR = "some value";
-#        });
-#      in
-#      {
-#        checks = {
-#          inherit metasearch2;
-#        };
-#
-#        packages.default = metasearch2;
-#
-#        apps.default = flake-utils.lib.mkApp {
-#          drv = metasearch2;
-#        };
-#		
-#		nixosModules.default = import ./module.nix inputs;
-#
-#        devShells.default = craneLib.devShell {
-#          # Inherit inputs from checks.
-#          checks = self.checks.${system};
-#
-#          # Additional dev-shell environment variables can be set directly
-#          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-#
-#          # Extra inputs can be added here; cargo and rustc are provided by default.
-#          packages = [
-#            # pkgs.ripgrep
-#          ];
-#        };
-#      });
 }
