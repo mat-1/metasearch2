2c099a2d189ed4d0e4c4f64db01578018b76ff83
caeklol <me@caek.dev>
2024-12-25 23:51:08 +0800

    add aarch64-linux to target systems

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
 
   outputs = inputs @ { self, crane, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-		systems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"];
+		systems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux"];
 
 		perSystem = {
 			pkgs,
