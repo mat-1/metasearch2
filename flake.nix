dbee112287dc94e5a9bd3bf524991e2228fe5690
caeklol <me@caek.dev>
2024-12-25 17:21:44 +0000

    flake-utils -> flake-parts

--- flake.nix
+++ flake.nix
@@ -6,64 +6,73 @@
 
     crane.url = "github:ipetkov/crane";
 
-    flake-utils.url = "github:numtide/flake-utils";
+	flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = inputs @ { self, crane, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+		systems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux"];
 
-        craneLib = crane.mkLib pkgs;
+		perSystem = {
+			pkgs,
+			system,
+			...
+		}: let
+			craneLib = crane.mkLib pkgs;
 
-        assetFilter = path: _type: (pkgs.lib.strings.hasPrefix (toString ./src/web/assets) path);
-        sourceFilter = path: type: (craneLib.filterCargoSources path type) || (assetFilter path type);
+			assetFilter = path: _type: (pkgs.lib.strings.hasPrefix (toString ./src/web/assets) path);
+			sourceFilter = path: type: (craneLib.filterCargoSources path type) || (assetFilter path type);
 
-        # Common arguments can be set here to avoid repeating them later
-        # Note: changes here will rebuild all dependency crates
-        commonArgs = {
-          src = pkgs.lib.cleanSourceWith {
-            src = ./.;
-            filter = sourceFilter;
-            name = "source"; # Be reproducible, regardless of the directory name
-          };
-          strictDeps = true;
+			# Common arguments can be set here to avoid repeating them later
+			# Note: changes here will rebuild all dependency crates
+			commonArgs = {
+			  src = pkgs.lib.cleanSourceWith {
+				src = ./.;
+				filter = sourceFilter;
+				name = "source"; # Be reproducible, regardless of the directory name
+			  };
+			  strictDeps = true;
 
-          buildInputs = [
-            # Add additional build inputs here
-          ];
-        };
+			  buildInputs = [
+				# Add additional build inputs here
+			  ];
+			};
 
-        metasearch2 = craneLib.buildPackage (commonArgs // {
-          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+			metasearch2 = craneLib.buildPackage (commonArgs // {
+			  cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-          # Additional environment variables or build phases/hooks can be set
-          # here *without* rebuilding all dependency crates
-          # MY_CUSTOM_VAR = "some value";
-        });
-      in
-      {
-        checks = {
-          inherit metasearch2;
-        };
+			  # Additional environment variables or build phases/hooks can be set
+			  # here *without* rebuilding all dependency crates
+			  # MY_CUSTOM_VAR = "some value";
+			});
+		in {
+			formatter = pkgs.alejandra;
 
-        packages.default = metasearch2;
+			checks = {
+				inherit metasearch2;
+			};
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = metasearch2;
-        };
+			packages.default = metasearch2;
 
-        devShells.default = craneLib.devShell {
-          # Inherit inputs from checks.
-          checks = self.checks.${system};
+			apps.default = {
+				type = "app";
+				program = "${self.packages.${system}.default}/bin/metasearch";
+			};
 
-          # Additional dev-shell environment variables can be set directly
-          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+			devShells.default = craneLib.devShell {
+			  checks = self.checks.${system};
 
-          # Extra inputs can be added here; cargo and rustc are provided by default.
-          packages = [
-            # pkgs.ripgrep
-          ];
-        };
-      });
+			  # Additional dev-shell environment variables can be set directly
+			  # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+			  # Extra inputs can be added here; cargo and rustc are provided by default.
+			  packages = [
+				# pkgs.ripgrep
+			  ];
+			};
+		};
+	};
 }
