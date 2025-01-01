fa839e3c53c44d7f9bfea962acaed49dbd047cec
mat <git@matdoes.dev>
2025-01-01 05:39:48 +0000

    show all headers in useragent answer

--- src/engines/answer/useragent.rs
+++ src/engines/answer/useragent.rs
@@ -13,11 +13,27 @@ pub fn request(query: &SearchQuery) -> EngineResponse {
 
     let user_agent = query.request_headers.get("user-agent");
 
+    let all_headers_html = html! {
+        br;
+        details {
+            summary { "All headers" }
+            @for (header, value) in query.request_headers.iter() {
+                div {
+                    b { (header) } ": " (value)
+                }
+            }
+        }
+    };
+
     EngineResponse::answer_html(if let Some(user_agent) = user_agent {
         html! {
             h3 { b { (user_agent) } }
+            (all_headers_html)
         }
     } else {
-        html! { "You don't have a user agent" }
+        html! {
+            "You don't have a user agent"
+            (all_headers_html)
+        }
     })
 }
